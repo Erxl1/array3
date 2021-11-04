aa32d7740adc48a9c1ac958e0f31e096641cb9d4
EROL <18duders@gmail.com>
2021-11-04 11:38:14 +0200

    TK_2.py fix

--- TK_2.py
+++ TK_2.py
@@ -1,6 +1,5 @@
 def get_min_max(list_data, list_size):
-    minim = list_data[0]
-    maxim = list_data[0]
+    minim, maxim = float(list_data[0])
     for i in range(list_size):
         if minim > list_data[i]:
             minim = list_data[i]
